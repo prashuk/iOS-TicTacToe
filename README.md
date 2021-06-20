cde8f2063937d05f7f2e53a5fc60117fdfe8ed27
Prashuk Ajmera <prashuk.ajmera@gmail.com>
2021-06-19 22:48:30 -0500

    Custom computer moves

---
+++ README.md
@@ -0,0 +1,6 @@
+# iOS-TicTacToe
+
+- A simple Tic-Tac-Toe game build on SwiftUI with MVVM design pattern
+- Computer moves determined by custom algortihm
+- Track yours and computer's win
+
--- TicTacToe/TicTacToe/ViewModel/GameViewModel.swift
+++ TicTacToe/TicTacToe/ViewModel/GameViewModel.swift
@@ -15,7 +15,6 @@ final class GameViewModel: ObservableObject {
     
     
     @Published var moves: [Move?] = Array(repeating: nil, count: 9)
-    @Published var isHumanTurn = true
     @Published var isGameBoardDisable = false
     @Published var alertItem: AlertItem?
     
@@ -27,7 +26,7 @@ final class GameViewModel: ObservableObject {
         // Human move processing
         if isSquareOccupied(in: moves, forIndex: position) { return }
         
-        moves[position] = Move(player: isHumanTurn ? .human : .computer, boardIndex: position)
+        moves[position] = Move(player: .human, boardIndex: position)
         
         if winningCondition(for: .human, in: moves) {
             humanWon = humanWon + 1
@@ -66,12 +65,48 @@ final class GameViewModel: ObservableObject {
     }
     
     func determineComputerMove(in moves: [Move?]) -> Int {
+        
+        // If AI can win, then win
+        let winPattern: Set<Set<Int>> = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
+        
+        let computerMoves = moves.compactMap{ $0 }.filter{ $0.player == .computer }
+        let computerPosition = Set(computerMoves.map{ $0.boardIndex })
+        
+        for pattern in winPattern {
+            let winPosition = pattern.subtracting(computerPosition)
+            
+            if winPosition.count == 1 {
+                let isAvailable = !isSquareOccupied(in: moves, forIndex: winPosition.first!)
+                if isAvailable { return winPosition.first!}
+            }
+        }
+        
+        // If AI can't win, then block
+        let humanMoves = moves.compactMap{ $0 }.filter{ $0.player == .human }
+        let humanPosition = Set(humanMoves.map{ $0.boardIndex })
+        
+        for pattern in winPattern {
+            let winPosition = pattern.subtracting(humanPosition)
+            
+            if winPosition.count == 1 {
+                let isAvailable = !isSquareOccupied(in: moves, forIndex: winPosition.first!)
+                if isAvailable { return winPosition.first!}
+            }
+        }
+        
+        // If AI can't block, then take the middle square
+        let center = 4
+        
+        if !isSquareOccupied(in: moves, forIndex: center) {
+            return center
+        }
+        
+        // If AI can't take middle square, take random available square
         var movePosition = Int.random(in: 0..<9)
         
         while (isSquareOccupied(in: moves, forIndex: movePosition)) {
             movePosition = Int.random(in: 0..<9)
         }
-        
         return movePosition
     }
     
